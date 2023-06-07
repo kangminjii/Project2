2b62128240a0a3cbcac7721bd248c79988069c83
Kang-MinJi <87412863+kangminjii@users.noreply.github.com>
2023-06-07 23:16:57 +0900

    Update 20230607_Q4_IntStack.cpp

--- Project2/20230607_Q4_IntStack.cpp
+++ Project2/20230607_Q4_IntStack.cpp
@@ -2,26 +2,18 @@
 #include <stdlib.h>
 #include "20230607_Q4.h"
 
-//typedef struct
-//{
-//	int max;
-//	int ptr;
-//	int* stk;
-//} IntStack;
-
 int Initialize(IntStack* s, int max)
 {
 	s->ptr = 0;
 	s->max = max;
 	s->stk = (int*)calloc(max, sizeof(int));
 
-	if (s->stk == NULL)		return -1; // ÇÒ´çÀÌ Àß¸øµÆ´Ù
+	if (s->stk == NULL)		return -1; // í• ë‹¹ì´ ìž˜ëª»ëë‹¤
 	else return 0;
 }
-
 int Push(IntStack* s, int x)
 {
-	if (s->ptr >= s->max)	return -1; // ¹è¿­ »çÀÌÁî¸¦ ³Ñ¾ú´Ù
+	if (s->ptr >= s->max)	return -1; // ë°°ì—´ ì‚¬ì´ì¦ˆë¥¼ ë„˜ì—ˆë‹¤
 	else
 	{
 		s->stk[s->ptr] = x;
@@ -29,10 +21,9 @@ int Push(IntStack* s, int x)
 		return 0;
 	}
 }
-
 int Pop(IntStack* s, int* x)
 {
-	if (s->ptr <= 0)	return -1; // ¹è¿­¿¡ ÀúÀåµÈ °ªÀÌ ¾ø´Ù
+	if (s->ptr <= 0)	return -1; // ë°°ì—´ì— ì €ìž¥ëœ ê°’ì´ ì—†ë‹¤
 	else
 	{
 		s->stk[s->ptr] = NULL;
@@ -41,25 +32,46 @@ int Pop(IntStack* s, int* x)
 		return 0;
 	}
 }
-
 int Peek(const IntStack* s, int* x)
 {
-	if (s->ptr <= 0)		return -1;
+	if (s->ptr <= 0)		return -1; // ë°°ì—´ì— ì €ìž¥ëœ ê°’ì´ ì—†ë‹¤
 	else
 	{
-		*x = s->stk[s->ptr];
+		*x = s->stk[s->ptr - 1];
 		return 0;
 	}
 }
-
-void Clear(IntStack* s);
-
-int Capacity(const IntStack* s);
-int Size(const IntStack* s);
-int IsEmpty(const IntStack* s);
-int IsFull(const IntStack* s);
-int Search(const IntStack* s, int x);
-
+void Clear(IntStack* s) // ê°€ë¦¬í‚¤ëŠ” ì¸ë±ìŠ¤ë¥¼ 0ìœ¼ë¡œ í•˜ë©´ ë°°ì—´ ê°’ì´ ëª¨ë‘ ì‚¬ë¼ì§„ë‹¤
+{
+	s->ptr = 0;
+}
+int Capacity(const IntStack* s)
+{
+	return s->max;
+}
+int Size(const IntStack* s)
+{
+	return s->ptr;
+}
+int IsEmpty(const IntStack* s)
+{
+	return s->ptr <= 0;
+}
+int IsFull(const IntStack* s)
+{
+	return s->ptr >= s->max;
+}
+int Search(const IntStack* s, int x)
+{
+	for (int i = 0; i < s->ptr; i++)
+	{
+		if (s->stk[i] == x)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
 void Print(const IntStack* s)
 {
 	for (int i = 0; i < s->ptr; i++)
@@ -68,8 +80,9 @@ void Print(const IntStack* s)
 	}
 	printf("\n");
 }
-
 void Terminate(IntStack* s)
 {
-	delete s;
+	if (s->stk != NULL)	free(s->stk);
+	s->max = 0;
+	s->ptr = 0;
 }
